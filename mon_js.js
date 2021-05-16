cc2aba49a6735ce09b6c7fd5ddefff2fc84a7918
happyvolt92 <elodiegage@gmail.com>
2021-05-16 12:41:56 +0200

    Delete mon_js.js

--- mon_js.js
+++
@@ -1,5 +0,0 @@
-
-
-// windows.fetch('https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}')
-// .then(res => res.json())
-// .then(resJson => console.log(resJson))
